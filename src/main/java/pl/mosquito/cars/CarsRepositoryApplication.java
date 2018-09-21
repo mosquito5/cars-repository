c078c0e10bcaf01a0f7bce4fd48d9634414028f7
Jakub <jakub.moskal3@gmail.com>
2018-09-21 22:29:26 +0200

    first commit

---
+++ src/main/java/pl/mosquito/cars/CarsRepositoryApplication.java
@@ -0,0 +1,12 @@
+package pl.mosquito.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsRepositoryApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/mosquito/cars/CarsRepositoryApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mosquito.cars;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarsRepositoryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
