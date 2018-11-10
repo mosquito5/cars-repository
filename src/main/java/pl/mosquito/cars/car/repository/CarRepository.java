e212084c40ab2decfdd51b631df4f894bc002270
Jakub <jakub.moskal3@gmail.com>
2018-11-10 13:07:13 +0100

    Added new findBy methods

--- src/main/java/pl/mosquito/cars/car/repository/CarRepository.java
+++ src/main/java/pl/mosquito/cars/car/repository/CarRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.mosquito.cars.car.model.Car;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
+    List<Car> findByBrand(String brand);
+    List<Car> findByModel(String model);
+    List<Car> findByUserName(String user);
+    List<Car> findByFuel(String fuel);
 }
